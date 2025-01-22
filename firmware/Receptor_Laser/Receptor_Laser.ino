26de13455555d77af13f6297c64f4df358d9feb4
morsoth <94766863+morsoth@users.noreply.github.com>
2025-01-22 13:33:21 +0100

    Update Receptor_Laser.ino

--- firmware/Receptor_Laser/Receptor_Laser.ino
+++ firmware/Receptor_Laser/Receptor_Laser.ino
@@ -1,4 +1,4 @@
-#include <cmath>
+#include <math.h>
 
 #include <Adafruit_MCP3008.h>
 
@@ -28,6 +28,8 @@ Adafruit_MCP3008 adc6;
 #define SCREEN_HEIGHT 128
 #define SCREEN_WIDTH 32
 
+#define SCREEN_ADDRESS 0x3C
+
 Adafruit_SSD1306 display(SCREEN_HEIGHT, SCREEN_WIDTH, &Wire, -1);
 
 #define TOTAL_PHOTODIODES 48
@@ -36,20 +38,30 @@ int photodiode_values[TOTAL_PHOTODIODES] = {0};
 void setup() {
   Serial.begin(115200);
 
+  pinMode(BUZZ, OUTPUT);
+
   if (!adc1.begin(CS1) || !adc2.begin(CS2) || !adc3.begin(CS3) || !adc4.begin(CS4) || !adc5.begin(CS5) || !adc6.begin(CS6)) {
     Serial.println(F("Error: No se pudo inicializar el MCP3008."));
     while (1);
   }
   Serial.println(F("MCP3008 inicializado correctamente."));
 
-  if (!display.begin(SSD1306_I2C_ADDRESS, 0x3C)) {
+  if (!display.begin(SCREEN_ADDRESS, 0x3C)) {
     Serial.println(F("Error: No se pudo inicializar la pantalla OLED."));
     while (1);
   }
   Serial.println(F("Pantalla OLED inicializada correctamente."));
 
+  display.setRotation(1);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+ 
   display.clearDisplay();
-  display.setRotation(3);
+  display.display();
+
+  digitalWrite(BUZZ, HIGH);
+  delay(200);
+  digitalWrite(BUZZ, LOW);
 }
 
 void loop() {
@@ -57,7 +69,7 @@ void loop() {
 
   calculateLaserPosition();
 
-  printPhotodiodeValues();
+  //printPhotodiodeValues();
 
   delay(100);
 }
@@ -75,36 +87,79 @@ void readADCs() {
 }
 
 void calculateLaserPosition() {
-  const int THRESHOLD = 600;
+  const int THRESHOLD = 650;
 
-  int maxValue = 0;
-  int maxPhotodiode = -1;
+  int max_value = 0;
+  int max_photodiode = -1;
 
   for (int ph = 0; ph < TOTAL_PHOTODIODES; ph++) {
-    if (photodiode_values[i] > THRESHOLD && photodiode_values[i] > max_value) {
-      max_value = photodiode_values[i];
-      max_index = i;
+    if (photodiode_values[ph] > THRESHOLD && photodiode_values[ph] > max_value) {
+      max_value = photodiode_values[ph];
+      max_photodiode = ph;
     }
   }
+    
+  display.clearDisplay();
 
-  if (maxPhotodiode != -1) {
-    int topPhotodiode, botPhotodiode;
+  display.drawLine(0, 64, 16, 64, SSD1306_WHITE);
+
+  if (max_photodiode != -1) {
+    int top_photodiode, bot_photodiode;
 
     // Left column
-    if (maxPhotodiode < TOTAL_PHOTODIODES / 2) {
-      topPhotodiode = TOTAL_PHOTODIODES - 1 - maxPhotodiode;
-      botPhotodiode = topPhotodiode + 1;
+    if (max_photodiode < TOTAL_PHOTODIODES / 2) {
+      top_photodiode = TOTAL_PHOTODIODES - 1 - max_photodiode;
+
+      if (max_photodiode != 0) bot_photodiode = top_photodiode + 1;
+      else bot_photodiode = -1;
     }
     // Right column
     else {
-      topPhotodiode = TOTAL_PHOTODIODES + 1 - maxPhotodiode;
-      botPhotodiode = topPhotodiode - 1;
+      bot_photodiode = TOTAL_PHOTODIODES - 1 - max_photodiode;
+      
+      if (max_photodiode != 24) top_photodiode = bot_photodiode + 1;
+      else top_photodiode = -1;
     }
 
-    // Adjust psotiion
+    if (max_photodiode > 16 && max_photodiode < 32) {
+      display.fillTriangle(16, 0, 0, 10, 31, 10, SSD1306_WHITE);
+    }
+    else if (max_photodiode < 5 || max_photodiode > 43) {
+      display.fillTriangle(16, 127, 0, 117, 31, 117, SSD1306_WHITE);
+    }
+    else {
+      display.setCursor(2,2);
+      display.println(max_photodiode);
+      /*
+      display.println(top_photodiode);
+      display.println(bot_photodiode);
+  
+      
+      display.setCursor(2,32);
+      display.println(photodiode_values[max_photodiode]);
+      display.println(photodiode_values[top_photodiode]);
+      display.println(photodiode_values[bot_photodiode]);
+*/
+      int height;
+      
+      if (max_photodiode < TOTAL_PHOTODIODES / 2) height = -11*max_photodiode + 176;
+      else height = 11*max_photodiode - 346;
+
+      display.drawLine(17, height, 32, height, SSD1306_WHITE);
+    }
+/*
+    if (max_photodiode == 37) {
+      digitalWrite(BUZZ, HIGH);
+      delay(200);
+      digitalWrite(BUZZ, LOW);
+    }
+    */
+    // Adjust position
 
     // Map the photodiode with the display height
   }
+
+  display.display();
 }
 
 void printPhotodiodeValues() {
@@ -112,4 +167,8 @@ void printPhotodiodeValues() {
     if (ph % 8 == 7) Serial.println((String)"PH" + ph + ": \t" + photodiode_values[ph]);
     else Serial.print((String)"PH" + ph + ": \t" + photodiode_values[ph] + ", \t");
   }
+  Serial.println("------------------------------------------------------------------");
 }
+
+// 16 - 5
+// 32 - 43
